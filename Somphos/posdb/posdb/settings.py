a057230fc5f92e14895d814d550d6ab1b4853c11
Somphos27 <somphorsphea211@gmail.com>
2026-05-11 00:44:00 +0700

    Update settings.py

--- Somphos/posdb/posdb/settings.py
+++ Somphos/posdb/posdb/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = '/home/Somphosphea/somphorsB204/Somphos/posdb/staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = '/sales/products/'
+LOGIN_REDIRECT_URL = '/sales/products_list/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 CSRF_TRUSTED_ORIGINS = [

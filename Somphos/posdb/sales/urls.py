58fde9193c696ef91689b9ea16d6460f78f11f64
Somphos27 <somphorsphea211@gmail.com>
2026-05-11 00:43:12 +0700

    Update urls.py

--- Somphos/posdb/sales/urls.py
+++ Somphos/posdb/sales/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'),
+    path('products_list/', views.product_list, name='product_list'),
     path('products/<int:pk>/', views.product_detail, name='product_detail'),
     path('orders/', views.order_list, name='order_list'),
     path('orders/new/', views.create_order, name='create_order'),
